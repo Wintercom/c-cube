254e21779eb47350aa2fa7fbafa81b9907b69feb
chennan <chennan@qiniu.com>
2025-10-26 23:53:29 +0800

    更新 README

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,6 +7,12 @@ export default defineConfig({
   server: {
     port: 5000, // 将端口改为 5000
     strictPort: true, // 端口被占用时直接退出，不尝试其他端口:cite[1]
+    proxy: {
+      "/api": {
+        target: "http://192.168.31.223:8080/",
+        changeOrigin: true
+      }
+    }
   },
   plugins: [
     vue(),
